e3361967ca85acc13685a865e892d8c551ac5890
DeWojtek <46491253+DeWojtek@users.noreply.github.com>
2019-01-08 15:47:30 +0100

    Add files via upload

---
+++ GraphApp/ip_file_valid.py
@@ -0,0 +1,31 @@
+import os.path
+import sys
+
+#Checking IP address file and content validity
+def ip_file_valid():
+	
+	#prompting user for info
+	ip_file = input("\n# Enter IP file path and name (e.g. D:\Python\lab\ip.txt):")
+	
+	#checking if the file exist
+	if os.path.isfile(ip_file) == True:
+		print("\n* IP file is valid:) \n")
+		
+	else:
+		print("\n File {} does not exist : (Please check and try again.\n".format(ip_file))
+		sys.exit()
+		
+	
+	#Open user selected file for reading (IP addresses file)
+	selected_ip_file = open (ip_file, 'r')
+	
+	#Starting from the beginning of the file
+	selected_ip_file.seek(0);
+	
+	#Reading each line (IP addresses) in the file
+	ip_list = selected_ip_file.readlines()
+	
+	#Closing the file
+	selected_ip_file.close()
+	
+	return ip_list
